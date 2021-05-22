7b872c0f5165bc844d7ba2a2c2ebdb13d19d30e0
Morgan Bligh <morganbligh@icloud.com>
2021-05-22 16:34:52 -0700

    HW 5

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import express from 'express';
 import handlebars from "express-handlebars"
 import { Human } from "../IT_122_javascript/models/human.js";
+import cors from 'cors';
 
 
 
@@ -18,6 +19,7 @@ app.use(express.json()); //Used to parse JSON bodies
 
 app.engine('hbs', handlebars({defaultLayout: "main.hbs"}));
 app.set("view engine", "hbs");
+app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
 
 // app.get('/', (req,res) => {
 //     res.render('home', {humans: data.getAll()});
@@ -104,6 +106,49 @@ app.get('/delete', (req,res) => {
         });
     });
 });
+
+//API ROUTES
+
+    app.get('/api/humans', (req,res, next) => {
+        Human.find({}).lean()
+            .then((humans) => {
+                res.json(humans)
+            })
+            .catch(err => next(err));
+
+        });
+
+
+
+    app.get('/api/human/:title', (req, res, next) => {
+        let surname = req.params.title;
+        Human.findOne({name: surname}, (err, result) => {
+            if (err || !result) return next(err);
+            res.json( result );    
+        });
+    });
+
+    app.get('/api/delete/:title', (req, res, next) => {
+        let surname = req.params.title;
+        Human.deleteOne({name: surname}, (err, result) => {
+            if (err || !result) return next(err);
+            res.json( result );    
+        });
+    });
+
+    app.get('/api/add/:title/:age/:color/:shape', (req,res, next) => {
+        let surname = req.params.title;
+        Human.updateOne({ name: surname}, { name:surname, age: req.params.age, color: req.params.color, shape: req.params.shape }, {upsert: true }, (err, result) => {
+            if (err) return next(err);
+            // nModified = 0 for new item, = 1+ for updated item 
+            res.json({updated: result.nModified});
+        });
+    });
+
+
+
+
+
 
 
 
